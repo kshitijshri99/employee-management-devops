d6d8f733af66f3f7f7dad5ba6963d56955c0f2ac
kshitijshri99 <kshitijshrivastava99@gmail.com>
2026-07-29 13:08:49 +0530

    Landing page added

--- Jenkinsfile
+++ Jenkinsfile
@@ -132,5 +132,3 @@ pipeline {
 
             cleanWs()
         }
-    }
-}
--- src/main/java/com/devops/employee/controller/HomeController.java
+++ src/main/java/com/devops/employee/controller/HomeController.java
@@ -1,13 +1,13 @@
 package com.devops.employee.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Employee Management System API is running successfully!";
+        return "index";
     }
 }
